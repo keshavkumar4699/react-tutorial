449083f29dfdb15d02a3f95b1c759d864fe8d965
keshavkumar4699 <kumar.keshav4699@gmail.com>
2024-08-24 17:43:57 +0530

    topic 23 components completed

--- 18 fragments/src/App.jsx
+++ 18 fragments/src/App.jsx
@@ -1,17 +1,33 @@
 import "./App.css";
+import Container from "./components/Container";
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
 
-// let foodItems = ["Fruits", "Ghee", "Vegetables", "Eggs", "Milk", "Pulses", "Noodles"];
-let foodItems = [];
+let foodItems = [
+  "Fruits",
+  "Ghee",
+  "Vegetables",
+  "Eggs",
+  "Milk",
+  "Pulses",
+  "Noodles",
+];
+// let foodItems = [];
 
 function App() {
-  
   return (
     <>
-      <h3 className="m-4 text-center">Healthy Foods</h3>
-      <ErrorMessage foodItems = {foodItems}></ErrorMessage>
-      <FoodItems foodItems = {foodItems}></FoodItems>
+      <Container>
+        <h3 className="m-4 text-center">Healthy Foods</h3>
+        <ErrorMessage foodItems={foodItems}></ErrorMessage>
+        <FoodItems foodItems={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>
+          Above is the list of foods that are good for your health and well
+          being
+        </p>
+      </Container>
     </>
   );
 }
